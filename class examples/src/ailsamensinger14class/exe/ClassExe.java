f16f7298a4cd5c3f0085e891039ae4ae5d15d198
ailsa.mensinger <ailsa.mensinger@CLC-LAB209-03.TCSD.tooelesd.org>
2018-09-10 14:09:43 -0600

    Class examples with ascii

---
+++ class examples/src/ailsamensinger14class/exe/ClassExe.java
@@ -0,0 +1,79 @@
+package ailsamensinger14class.exe;
+/*
+ * Ailsa
+ * 9-10-18
+ */
+
+public class ClassExe {
+
+	public static void main(String[] args) {
+		//print a single line
+		System.out.println("HH HH\tIIIII");
+		System.out.println("HHHHH HH\tIIII");
+		System.out.println("H HH\tIIII");
+		
+		System.out.println();
+		
+		
+		System.out.println("--------  ___@");
+		System.out.println("------  _`\\<,_");
+		System.out.println("----  (*)/   (*)");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~");
+		
+		System.out.println();
+		
+		
+		System.out.println(" __");
+		System.out.println("( 0>");
+		System.out.println("///\\");
+		System.out.println("\\V_/_");
+		
+		System.out.println();
+		
+		System.out.println("      #                                                                                                 ");
+		System.out.println("      #       ##      #    #     ##      #    ####     #######  #       #  #    #       ");
+		System.out.println("      #     #   #     #    #    #  #     #  #          #        #       #  ##   #    ");
+		System.out.println("      #    #     #    #    #   #    #    #    ####     #######  #       #  # #  #                   ");
+		System.out.println("#     #    #######    #    #   ######    #        #    #        #       #  #  # #         ");
+		System.out.println("#     #    #     #     #  #    #    #    #  #     #    #        #       #  #   ##        ");
+		System.out.println(" ####      #     #      ##     #    #    #   #####     #          #####    #    #     ");
+		
+		System.out.println();
+		
+		
+		System.out.println("MMMMMMMMMMMMMMWXKKXXXXXNNNNNNNNXXKXWMMMMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMMWOc,,;ccc:clloxO0Odc;cKMMMMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMWKo,...',,..',:xKX0d:,'cKMMMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMKl,....'....'';dKNXOl:,.:KMMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMWk;.  ......,lodkO0Okc...'OMMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMWk;..     .:oxxkkkkd:....;OMMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMO:'..   .cdxxxkxo:'..'..;OMMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMKoc'....;ooolc:,....od;,l0MMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMM0lc;''.............lOocxd0MMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMM0ol:.... ...;;,,o:.oOdkdl0MMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMKo;,;;c:.:xo:,,oO;.xkdOxl0MMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMKxccd::xcck;  .xk''docoll0MMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMM0dodo,cdcdd.  .:,..:ldddxKMMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMKdodo:c;;c,......,cdxxdoo0MMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMXdcol,,,;;,'...',:coooooo0MMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMXo::;:cccc;,''';:cloooood0MMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMXd:cccc::::;,,;:lllloooodKMMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMXdll::::ccc:::clloolllllo0MMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMXo:::::ccccllllooooolccox0WMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMXo;:::cccccllllooooollldkKWMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMXo;:::cccllloooodddddoldxKWMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMWx:::::ccloododdddxddooldKMMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMMN0kdooodddddddxxxxxxkOKNMMMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMMMMWNK00000000000KKXXNWMMMMMMMMMMMMMMM\r\n" + 
+				"MMMMMMMMMMMMMMMMMMMMWWNNNNNWWWMMMMMMMMMMMMMMMMMMMM");
+		
+		
+		
+	
+	
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
